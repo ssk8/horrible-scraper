3d4321ee1b477fc5d690ce6717fd73a6489fbf6a
ssk8 <stinkyskate@pm.me>
2019-09-12 19:19:17 -0500

    moved body to main

--- horrible_rename.py
+++ horrible_rename.py
@@ -9,6 +9,7 @@ def get_args():
     parser.add_argument('-p', '--path', default='/home/osmc/')
     parser.add_argument('-i', '--input_dir', default='Downloads')
     parser.add_argument('-o', '--output_dir', default='TV Shows')
+    parser.add_argument('-n', '--not_a_test', action='store_true')
     return parser.parse_args()
 
 
@@ -20,17 +21,15 @@ def find_files(home, input_dir, header):
 
 def get_new_name(old_name, header):
     current_name = old_name.replace(header, '')
-    season_search = re.search("S[0-9] ", current_name) #fix for S9+
+    season_search = re.search("S[0-9]{1,2} ", current_name)
     if season_search:
         current_season = current_name[season_search.start()+1]
+        current_name = current_name.replace(' S{}'.format(current_season), '')
     else:
         current_season = 1
-    name_index = re.search(r" - [0-9]{2} \[", current_name).start()
+    name_index = re.search(r" - [0-9]{1,2} [\[, (]", current_name).start()
     current_dir_name = current_name[0:name_index]
     current_name = '{} - S0{}E{}'.format(current_name[:name_index], current_season, current_name[name_index + 3:])
-    if season_search:
-        current_name = current_name.replace(' S{}'.format(current_season), '')
-        current_dir_name = current_dir_name.replace(' S{}'.format(current_season), '')
     return current_name, current_dir_name
 
 
@@ -39,16 +38,28 @@ def check_dir(directory):
         os.mkdir(directory)
 
 
-if __name__ == "__main__":
+def main():
     cli_args = get_args()
     path, input_dir, output_dir = cli_args.path, cli_args.input_dir, cli_args.output_dir
-    files = find_files(path, input_dir, cli_args.torrent_header)
+    t_header = cli_args.torrent_header
+
+    print('File name header: "{}"\n'.format(t_header))
+    files = find_files(path, input_dir, t_header)
 
     for file in files:
-        new_file_name, new_dir_name = get_new_name(file, cli_args.torrent_header)
-        #print(f'OLD: {file}\nNEW: "{new_file_name}" in dir "{new_dir_name}"\n')
-        print('OLD: {}\nNEW: "{}" in dir "{}"\n'.format(file, new_file_name, new_dir_name))
+        new_file_name, new_dir_name = get_new_name(file, t_header)
         new_dir = os.path.join(path, output_dir, new_dir_name)
-        check_dir(new_dir)
-        os.rename(os.path.join(path, input_dir, file), os.path.join(new_dir, new_file_name))
-        
+        print('OLD: {}\nNEW: "{}"\n    in "{}"\n'.format(file, new_file_name, new_dir))
+
+        if cli_args.not_a_test:
+            check_dir(new_dir)
+            os.rename(os.path.join(path, input_dir, file), os.path.join(new_dir, new_file_name))
+
+    if not cli_args.not_a_test:
+        print('***This was just a test! use "-n" flag to write***')
+
+
+if __name__ == "__main__":
+    main()
+
+
